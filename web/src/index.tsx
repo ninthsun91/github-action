31b5d3522b2c845ec6d4294ab87c372148f1e354
ninthsun91 <ninthsun91@gmail.com>
2023-04-30 01:08:32 +0900

    4

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App: React.FC = () => {
-  return <div>Hello World</div>;
+  return <div>Hello World4</div>;
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
